28324e57ab5d82dd4360f87de7819cfea99dc244
Laurie Frank <laurie360@hotmail.co.uk>
2019-12-03 11:59:47 +0000

    lab complete

--- src/main/java/com/codeclan/example/employeeservice/models/Employee.java
+++ src/main/java/com/codeclan/example/employeeservice/models/Employee.java
@@ -1,7 +1,11 @@
 package com.codeclan.example.employeeservice.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -12,8 +16,11 @@ public class Employee {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "age")
     private int age;
@@ -24,11 +31,36 @@ public class Employee {
     @Column(name = "email")
     private String email;
 
-    public Employee(String name, int age, int employeeNumber, String email) {
-        this.name = name;
+    @JsonIgnoreProperties("employees")
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @JsonIgnoreProperties("employees") //removed value = in case it breaks later
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            }
+    )
+    private List<Project> projects;
+
+    public Employee(String firstName, String lastName, int age, int employeeNumber, String email, Department department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
         this.employeeNumber = employeeNumber;
         this.email = email;
+        this.department = department;
+        this.projects = new ArrayList<>();
     }
 
     public Employee(){}
@@ -41,12 +73,20 @@ public class Employee {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getAge() {
@@ -71,5 +111,25 @@ public class Employee {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public void addProject(Project projects){
+        this.projects.add(projects);
     }
 }
--- src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
+++ src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
@@ -1,7 +1,11 @@
 package com.codeclan.example.employeeservice;
 
+import com.codeclan.example.employeeservice.models.Department;
 import com.codeclan.example.employeeservice.models.Employee;
+import com.codeclan.example.employeeservice.models.Project;
+import com.codeclan.example.employeeservice.repositories.DepartmentRepository;
 import com.codeclan.example.employeeservice.repositories.EmployeeRepository;
+import com.codeclan.example.employeeservice.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,14 +16,41 @@ class EmployeeserviceApplicationTests {
 	@Autowired
 	EmployeeRepository employeeRepository;
 
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void createEmployee(){
-		Employee sergei = new Employee("Sergei Poliivov", 32, 2213, "sergei.workaholic@sminoff.rs");
+	public void createEmployeeAndDepartment(){
+		Department hr = new Department("HR");
+		departmentRepository.save(hr);
+
+		Employee sergei = new Employee("Sergei", "Poliivov", 32,
+				2213, "sergei.workaholic@sminoff.rs", hr);
 		employeeRepository.save(sergei);
 	}
+
+	@Test
+	public void addEmployeeAndProject(){
+		Department hr = new Department("HR");
+		departmentRepository.save(hr);
+
+		Employee sergei = new Employee("Sergei", "Poliivov", 32,
+				2213, "sergei.workaholic@sminoff.rs", hr);
+		employeeRepository.save(sergei);
+
+		Project codeclan = new Project("PSD", 4);
+
+		codeclan.addEmployee(sergei);
+		projectRepository.save(codeclan);
+	}
+
+
 
 }
